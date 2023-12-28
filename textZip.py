3ab3ee95313c33b5524a11121d6e2599f8a03843
harabera <52222189+harabera@users.noreply.github.com>
2023-12-28 18:24:49 +0100

    Additional python library review

--- textZip.py
+++ textZip.py
@@ -1,14 +1,23 @@
 #Zipfile Module
 import zipfile
 #Open and List
-zip = zipfile.ZipFile("purchase.zip", "r")
+zip = zipfile.ZipFile("wishlist.zip", "r")
 print(zip.namelist)
 
 #MetaData
+for meta in zip.infolist():
+    print(meta)
 
+info = zip.getinfo("purchased.txt")
+print(info)
 
 #Accessing files in the zip folder
+print(zip.read("wishlist.txt"))
+with zip.open("wishlist.txt") as f:
+    print(f.read())
+
 
 #Extracting files in the zip folder
-
+zip.extract("wishlist.txt")
 #Closing files in the zip folder
+zip.close()
